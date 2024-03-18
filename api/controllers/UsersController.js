03d1f4ce40b6222443b51334778c922cc59f2e5f
Rashnotech <rashnotech@gmail.com>
2024-03-18 06:19:13 +0100

    Handle user registration

--- api/controllers/UsersController.js
+++ api/controllers/UsersController.js
@@ -1,45 +1,55 @@
-import Mailer from "../services/MailService";
-import User from "../models/User";
-import bcrypt from "bcrypt";
+import Mailer, { isMobile } from "../services/MailService.js";
+import sha1 from 'sha1';
+
+const validateInput = (input, requiredFields, res) => {
+    for (const field in requiredFields) {
+        const type = requiredFields[field];
+        if (!type) {
+            delete input[field];
+        } else if (type === 'required' && !input[field]) {
+            throw new Error(`${field} is required`);
+        }
+    }
+}
+
 
 class UsersController {
-    static async register (req, res, next) {
+
+    static async register (req, res) {
         const data = req.body;
-        
-        const requiredFields = [
-            'firstname', 'lastname',
-            'email', 'telephone',
-            'password', 'type', 'address'
-        ];
 
-        this.validateInput(data, requiredFields);
-        if (!data.email.includes('@')) return next(errorHandler(400, 'Invalid email address'));
-        const mobile = Mailer.isMobile(data.telephone);
-        if (!mobile) return next(errorHandler(400, 'Invalid phone number'));
-        const user = await User.findOne({ $or: [{ email }, { mobile }] });
-        if (!user) return next(errorHandler(400, 'User already exist'));
-        const token = Mailer.generateToken();
-        const response = await Mailer.sms(mobile, `Welcome to Rev platform. Your otp is ${token}`);
-        if (response.error) {
-            const res = await Mailer.mail(email, {
-                title: 'RevTax',
-                body: `<p>Welcome to Rev platform. Your otp is ${token} </p>`
-            });
-            if (res.error) return next(errorHandler(500, "An error occured while sending otp"));
-        }
-        data['password'] = bcrypt.hash(data.password, 10);
-        data['telephone'] = mobile;
-        const newUser = new User({ ...data});
-        await newUser.save();
-        return res.status(201).json({ message: 'User created successfully' });   
-    }
-
-    validateInput (input, requiredFields) {
-        for (const key in input) {
-            if (key in requiredFields && input.hasOwnProperty(key)) {
-                const element = input[key];
-                if (!element) return next(errorHandler(400, `${key} is required`));
+        const requiredFields = {   
+            firstname: 'required',
+            lastname: 'required',
+            email: 'required',
+            telephone: 'required',
+            password: 'required',
+            type: 'required',
+            address: 'required'
+        };
+        try {
+            validateInput(data, requiredFields, res);
+            if (!data.email.includes('@')) return res.status(400).json({error: 'Invalid email address'});
+            const mobile = isMobile(data.telephone);
+            if (!mobile) return res.status(400).json({error: 'Invalid phone number'});
+            const user = await User.findOne({ $or: [{ email }, { mobile }] });
+            if (user) res.status(400).json({error: 'User already exist'});
+            const token = Mailer.generateToken();
+            const response = await Mailer.sms(mobile, `Welcome to Rev platform. Your otp is ${token}`);
+            if (response.error) {
+                const mailResponse = await Mailer.mail(email, {
+                    title: 'RevTax',
+                    body: `<p>Welcome to Rev platform. Your otp is ${token} </p>`
+                });
+                if (mailResponse.error) return res.status(500).json({error:"An error occured while sending otp"});
             }
+            data.password = sha1(data.password);
+            data.telephone = mobile;
+            const newUser = new User({ ...data});
+            await newUser.save();
+            return res.status(201).json({ message: 'User created successfully' });
+        } catch (error) {
+        return res.status(400).json({error: error.message}); 
         }
     }
 }
--- api/server.js
+++ api/server.js
@@ -1,11 +1,20 @@
 import express from 'express';
-import router from './routes/index'
+import router from './routes/index.js'
 
 const app = express()
 const port = 5000
 
 app.use(express.json());
 app.use(router);
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message
+    });
+});
 app.listen(port);
 
 export default app;
