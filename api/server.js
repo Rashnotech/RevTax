0141e253ee3e957cbd1b060c37dc1adb935e3846
Inialpha <inimfonebong001@gmail.com>
2024-03-22 16:42:59 +0000

    Add a route for documentation

--- api/server.js
+++ api/server.js
@@ -2,6 +2,8 @@ import express from 'express';
 import router from './routes/index.js'
 import cors from 'cors'
 import mongoose from './utils/db.js'
+import swaggerUi from 'swagger-ui-express';
+import specs from './swagger.js';
 
 const app = express()
 const port = 5000
@@ -9,7 +11,9 @@ const port = 5000
 app.use(express.json());
 app.use(cors());
 app.use(router);
-  
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs))  
+
 app.listen(port, () => {
   console.log(`server running on localhost:${port}`)
 });
