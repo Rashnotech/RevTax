d283e5894bd5b8baeef5c38f154d056b780142af
Inialpha <inimfonebong001@gmail.com>
2024-03-22 19:49:06 +0000

    Add docummentstions for other user routes

--- api/routes/index.js
+++ api/routes/index.js
@@ -8,8 +8,135 @@ import AuthController from "../controllers/AuthController.js"
 
 const router = Router();
 
+/**
+ * @swagger
+ * openapi: 3.0.0
+ * info:
+ *   title: User Authentication API
+ *   description: API for user authentication and signup
+ *   version: 1.0.0
+ * servers:
+ *   - url: http://localhost:5000
+ * paths:
+ *   /auth/signup:
+ *     post:
+ *       summary: User Signup
+ *       description: |
+ *         Endpoint for user registration.
+ *       requestBody:
+ *         required: true
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 firstname:
+ *                   type: string
+ *                   description: The first name of the user
+ *                   example: Inimfon
+ *                 lastname:
+ *                   type: string
+ *                   description: The last name of the user
+ *                   example: Ebong
+ *                 telephone:
+ *                   type: string
+ *                   description: The telephone number of the user
+ *                   example: "09168848808"
+ *                 email:
+ *                   type: string
+ *                   format: email
+ *                   description: The email address of the user
+ *                   example: ebonginimfon7@gmail.com
+ *                 password:
+ *                   type: string
+ *                   description: The password for user authentication
+ *                   example: inimfon
+ *                 type:
+ *                   type: integer
+ *                   description: Type of user
+ *                   example: 1
+ *                 address:
+ *                   type: string
+ *                   description: The address of the user
+ *                   example: Cross river state
+ *       responses:
+ *         '200':
+ *           description: User created successfully
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 type: object
+ *                 properties:
+ *                   message:
+ *                     type: string
+ *                     description: Confirmation message
+ *                     example: User created successfully
+ *         '400':
+ *           description: Bad Request - Invalid input or missing required fields
+ *         '409':
+ *           description: Conflict - User with provided email already exists
+ *         '500':
+ *           description: Internal Server Error - Something went wrong while processing the request
+ *       security: []
+ */
 
 router.post('/auth/signup', UsersController.register);
+
+/**
+ * @swagger
+ * openapi: 3.0.0
+ * info:
+ *   title: User Authentication API
+ *   description: API for user authentication and signup
+ *   version: 1.0.0
+ * servers:
+ *   - url: http://localhost:5000
+ * paths:
+ *   /auth/login:
+ *     post:
+ *       summary: User Login
+ *       description: |
+ *         Endpoint for user authentication and login.
+ *       requestBody:
+ *         required: true
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 telephone:
+ *                   type: string
+ *                   description: The telephone number of the user
+ *                   example: "09168848807"
+ *                 email:
+ *                   type: string
+ *                   format: email
+ *                   description: The email address of the user
+ *                   example: ebonginimfon8@gmail.com
+ *                 password:
+ *                   type: string
+ *                   description: The password for user authentication
+ *                   example: inimfon
+ *       responses:
+ *         '200':
+ *           description: Successful login
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 type: object
+ *                 properties:
+ *                   token:
+ *                     type: string
+ *                     description: JWT token for authentication
+ *                     example: eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1ZmRkYmVjOThmMzY2MmJlZWU2NGM0MCIsImlhdCI6MTcxMTEzNjA0NywiZXhwIjoxNzEzNzI4MDQ3fQ.-TreDaCTx_dmkgXteRum6V9oj4qA-_7GaUM1IYdnLFratapF9YQsyMt14bGT1xohy112i_k_I7073W9wXSNkSw
+ *         '400':
+ *           description: Bad Request - Invalid input or missing required fields
+ *         '401':
+ *           description: Unauthorized - Invalid email, telephone, or password
+ *         '500':
+ *           description: Internal Server Error - Something went wrong while processing the request
+ *       security: []
+ */
 
 router.post('/auth/login', UsersController.login);
 
@@ -40,7 +167,7 @@ router.post('/auth/login', UsersController.login);
  *         '401':
  *           description: Unauthorized - Invalid or expired token
  *       security:
- *         - APIKeyHeader: []
+ *         - APIKeyAuth: []
  * components:
  *   schemas:
  *     User:
@@ -86,14 +213,113 @@ router.post('/auth/login', UsersController.login);
  *           type: integer
  *           description: Version control field
  *   securitySchemes:
- *     APIKeyHeader:
+ *     ApiKeyAuth:
  *       type: apiKey
  *       in: header
  *       name: Authorization
  */
 
 router.get('/users', jwtAuth, UsersController.getAllUsers);
+
+/**
+ * @swagger
+ * openapi: 3.0.0
+ * info:
+ *   title: User Management API
+ *   description: API for managing users
+ *   version: 1.0.0
+ * servers:
+ *   - url: http://localhost:5000
+ * paths:
+ *   /users/{userId}:
+ *     get:
+ *       summary: Get user by ID
+ *       description: |
+ *         Retrieves a specific user by their ID.
+ *       parameters:
+ *         - name: userId
+ *           in: path
+ *           description: The ID of the user to retrieve
+ *           required: true
+ *           schema:
+ *             type: string
+ *           example: 65fc22ad432b9300e6dd813c
+ *         - name: Authorization
+ *           in: header
+ *           description: Access token obtained after authentication
+ *           required: true
+ *           schema:
+ *             type: string
+ *           example: eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1ZmQzODNjZWI5MmNjYWUwNGExMGI4OCIsImlhdCI6MTcxMTA5MzkwMiwiZXhwIjoxNzEzNjg1OTAyfQ.g8kITz9UO0PHFkHnCxBfFa4DaBQ900m-99nJ5kJN7UrNU9Uj6cC8Y_SwRMpO2LI4-ly6ITo0w4m_EHQc6QjNqA
+ *       responses:
+ *         '200':
+ *           description: Successful response
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 $ref: '#/components/schemas/User'
+ *         '401':
+ *           description: Unauthorized - Invalid or expired token
+ *         '404':
+ *           description: Not Found - User with the specified ID not found
+ *         '500':
+ *           description: Internal Server Error - Something went wrong while processing the request
+ *       security:
+ *         - ApiKeyAuth: []
+ * components:
+ *   schemas:
+ *     User:
+ *       type: object
+ *       properties:
+ *         _id:
+ *           type: string
+ *           description: The unique identifier of the user
+ *         firstname:
+ *           type: string
+ *           description: The first name of the user
+ *         lastname:
+ *           type: string
+ *           description: The last name of the user
+ *         telephone:
+ *           type: string
+ *           description: The telephone number of the user
+ *         email:
+ *           type: string
+ *           format: email
+ *           description: The email address of the user
+ *         token:
+ *           type: string
+ *           description: Access token associated with the user
+ *         validated:
+ *           type: boolean
+ *           description: Indicates whether the user has been validated
+ *         type:
+ *           type: integer
+ *           description: Type of user
+ *         address:
+ *           type: string
+ *           description: The address of the user
+ *         createdAt:
+ *           type: string
+ *           format: date-time
+ *           description: The date and time when the user was created
+ *         updatedAt:
+ *           type: string
+ *           format: date-time
+ *           description: The date and time when the user was last updated
+ *         __v:
+ *           type: integer
+ *           description: Version control field
+ *   securitySchemes:
+ *     ApiKeyAuth:
+ *       type: apiKey
+ *       in: header
+ *       name: Authorization
+ */
+
 router.get('/users/:userId', jwtAuth, UsersController.getUser);
+
+
 router.put('/users/:userId', jwtAuth, UsersController.updateUser)
 router.delete('/users/:userId', jwtAuth, UsersController.deleteUser);
 
