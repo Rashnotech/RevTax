ca346e3340ee2b6c887efab667e7de77c285887f
Rashnotech <rashnotech@gmail.com>
2024-03-23 14:36:23 +0100

    Implemented history

--- view/src/pages/clients/history.tsx
+++ view/src/pages/clients/history.tsx
@@ -1,7 +1,27 @@
+import Table from "../../components/dashboard/table"
+
 const Transacthistory = () => {
     return (
         <section className="flex-1 w-full px-6 font-light">
-            
+            <h2 className="text-xl font-light mt-4 text-slate-600">Transaction History</h2>
+            <div className="p-4 mt-3 flex items-center justify-between bg-white shadow-sm">
+                <label className="relative block w-1/2">
+                    <span className="sr-only">Search</span>
+                    <span className="absolute inset-y-0 left-0 flex items-center pl-2">
+                        <svg className="h-5 w-5 fill-slate-300" viewBox="0 0 20 20"></svg>
+                    </span>
+                    <input className="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="Search for anything..." type="text" name="search"/>
+                </label>
+                <div className="space-x-4">
+                    <button className="rounded-md text-sm border px-4 py-2 bg-sky-700 text-white hover:bg-sky-500">View receipt</button>
+                    <button className="rounded-md bg-primary text-sm border text-white bg-pink-700 hover:bg-pink-600 px-4 py-2">Download Receipt</button>
+                </div>
+            </div>
+
+            <Table
+                caption="Revenue transaction history"
+                head={['S/N', 'Amount', 'Method', 'Date', 'Status']}
+                body={[]} />
         </section>
     )
 }
