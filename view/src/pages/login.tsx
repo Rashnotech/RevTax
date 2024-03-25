05a21beed9ad24c96745edcf6c7436538ff97dd7
Rashnotech <rashnotech@gmail.com>
2024-03-25 13:13:34 +0100

    Revamp auth loader

--- view/src/pages/login.tsx
+++ view/src/pages/login.tsx
@@ -3,13 +3,15 @@ import { useState } from 'react';
 import resolver from '../utils/resolverLog';
 import { useForm, SubmitHandler } from 'react-hook-form';
 import { IFormInput } from '../utils/types';
-import PostRequest from '../utils/PostRequest'
+import { UsersRequest } from '../utils/PostRequest'
 import { useNavigate } from 'react-router-dom'
+import Feedback from '../components/alert';
 
 function Login() {
     const navigate = useNavigate();
     const [loading, setLoading] = useState(false)
-    const [feedback, setFeedback] = useState()
+    const [feedback, setFeedback] = useState('')
+    const [error, setError] = useState('')
     const {
         register,
         watch,
@@ -20,31 +22,29 @@ function Login() {
     const password = watch('password');
 
     const onSubmit: SubmitHandler<IFormInput> = async (data) => {
-        /**
-         * PostRequest goes in here
-         * for url use the import.meta.env.url variable in the .env file
-         * 
-         */
-
-	const url = `${import.meta.env.VITE_API_URL}/login`
-        
-        const response = await PostRequest(url, data)
-	
-	if (response.ok) {
-	  const json = await response.json()
-	  const token = json.token
-	  const expire = new Date()
-	  expire.setTime(expire.getTime() + (30 * 24 * 60 * 60 * 1000))
-	  document.cookie = `rev_tax=${token};expires=${expire.toUTCString()};path=/`;
-	  setTimeout(() => {
-        navigate('/dashboard')
-        }, 5000);
-    }
-
+        setLoading(true)
+        const url = `${import.meta.env.VITE_AUTH_URL}/login`
+        const response = await UsersRequest(url, data)
+        if (response.ok) {
+            const res = await response.json()
+            const token = res.token
+            const expire = new Date()
+            expire.setTime(expire.getTime() + (30 * 24 * 60 * 60 * 1000))
+            document.cookie = `rev_tax=${token};expires=${expire.toUTCString()};path=/`;
+            setFeedback('Redirecting to dashboard...')
+            setTimeout(() => {
+                navigate('/user/dashboard')
+            }, 5000);
+        } else {
+            setError('Invalid email or password');
+        }
+        setLoading(false)
     };
     return (
         <div className="form_container md:w-3/5 mx-auto">
             <section className="form_wrapper">
+                {feedback && <Feedback message={feedback} status='success' />}
+                {error && <Feedback message={error} status='error' />}
                 <h2 className="text-2xl font-light">Welcome</h2>
                 <p className="text-slate-900 font-light">Log in to RevTax to pay your tax</p>
                 <form onSubmit={handleSubmit(onSubmit)} className="w-full space-y-6">
@@ -76,7 +76,7 @@ function Login() {
                         {errors?.password && <span className='text-red-500 text-xs'>{errors.password.message}</span>}
                     </div>
                     <a href='/reset_password' className='text-blue-700 font-light mt-4 text-sm cursor-pointer'>Forget password?</a>
-                    <button className='btn_primary'>continue</button>
+                    <button className='btn_primary' disabled={loading}>continue</button>
                     <p className='text-sm font-light'>Don&apos;t have an account? <a href="/signup" className='text-blue-700 cursor-pointer'>Sign up</a></p>
                 </form>
             </section>
