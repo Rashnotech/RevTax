de78b8871deef382c42c56ac0afb0c2e9b51d2da
Inialpha <inimfonebong001@gmail.com>
2024-03-27 07:38:41 +0000

    Add validation for business type

--- api/models/business.js
+++ api/models/business.js
@@ -10,6 +10,20 @@ const businessSchema = new Schema({
   LGA: { type: String, required: true }
 }, { timestamps: true });
 
+businessSchema.methods = {
+  isValidate: async function(type) {
+    const BusinessType = mongoose.model('BusinessType')
+    const businessTypes = await BusinessType.find({})
+    const typeList = businessTypes.map(obj => obj['name'])
+    return typeList.includes(type)
+  }
+}
+
+businessSchema.path('type').validate(function(type) {
+  return this.isValidate(type);
+}, "Invalid business type")
+  
+
 const Business = mongoose.model('Business', businessSchema);
 
 export default Business;
