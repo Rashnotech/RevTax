667b9b4fddbbaced28fc4b41c42b5c0af1487710
Initech <inimfonebong001@gmail.com>
2024-03-30 14:10:14 +0000

    Add a put request handler

--- view/src/utils/GetRequest.ts
+++ view/src/utils/GetRequest.ts
@@ -7,8 +7,25 @@ export const getRequest = async (url: string) => {
         {headers: new Headers({
             'Content-Type': 'application/json',
 	    'Authorization': cookie,
+	    'Cache-Control': 'no-cache',
         }),
         method: 'GET'
+    })
+    return response
+}
+
+
+export const putRequest = async (url: string, data: any) => {
+	const cookie = getCookie('rev_tax')
+	alert(cookie)
+    const response = await fetch(url,
+        {headers: new Headers({
+	    'Content-Type': 'application/json',
+            'Authorization': cookie,
+	    'Cache-Control': 'no-cache',
+        }),
+	method: 'PUT',
+	body: JSON.stringify(data),
     })
     return response
 }
