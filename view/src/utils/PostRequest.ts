597fc91d32b80e105aa5c399b92f85c769c63601
Initech <inimfonebong001@gmail.com>
2024-03-31 14:29:50 +0000

    src/utils/PostRequest.ts

--- view/src/utils/PostRequest.ts
+++ view/src/utils/PostRequest.ts
@@ -2,7 +2,6 @@ import { getCookie } from './cookiemanager';
 
 export const UsersRequest = async (url: string, data: any) => {
     const cookie = getCookie('rev_tax')
-    alert(cookie)
     const response = await fetch(url,
         {headers: new Headers({
             'Content-Type': 'application/json',
